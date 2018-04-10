c9b63ba8f5edfb1c56db78c745b8d5f310bbd519
artur.musayelyan <musayelyan099@gmail.com>
2018-04-10 18:53:23 +0400

    Initial commit

---
+++ app/src/main/java/com/example/arturmusayelyan/preferencescreen/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.arturmusayelyan.preferencescreen;
+
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
+    }
+
+    public static class SettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.app_preferences);
+        }
+
+
+    }
+}
